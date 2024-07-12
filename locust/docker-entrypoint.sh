5260df9790f7392a1537536914df47b49d0c9530
Tarun Menon <tarunmenon95@gmail.com>
2024-07-12 11:33:10 +1000

    create new build for distributed locust

--- locust/docker-entrypoint.sh
+++ locust/docker-entrypoint.sh
@@ -1,35 +1,17 @@
 #!/bin/sh
 
-set -ex
+set -e
 
-aws s3 cp s3://${LOCUST_S3_PATH} /locust/ --recursive --region ${AWS_REGION}
-
-LOCUST_MODE=${LOCUST_MODE:-standalone}
-LOCUST_MASTER_BIND_PORT=${LOCUST_MASTER_BIND_PORT:-5557}
-LOCUST_FILE=${LOCUST_FILE:-locustfile.py}
-LOCUST_LOG_LEVEL=${LOCUST_LOG_LEVEL:-INFO}
-
-LOCUST_OPTS="-f ${LOCUST_FILE}"
-
-if [ -z ${HOST_URL+x} ] ; then
-    echo "No value set for (HOST_URL), falling back to host value in the locust class"
-else
-    echo "(HOST_URL) set to ${HOST_URL}"
-    LOCUST_OPTS="--host=${HOST_URL} $LOCUST_OPTS"
+if [ "x" != "${LOCUST_S3_PATH}x" ] ; then
+    aws s3 cp s3://${LOCUST_S3_PATH}/${LOCUST_LOCUSTFILE} . --region ${AWS_REGION}
+    chmod +x ${LOCUST_LOCUSTFILE}
 fi
 
-case `echo ${LOCUST_MODE} | tr 'a-z' 'A-Z'` in
-"MASTER")
-    LOCUST_OPTS="--master --master-bind-port=${LOCUST_MASTER_BIND_PORT} $LOCUST_OPTS"
-    ;;
-
-"SLAVE")
-    LOCUST_OPTS="--worker --master-host=${LOCUST_MASTER} --master-port=${LOCUST_MASTER_BIND_PORT} $LOCUST_OPTS"
-    if [ -z ${LOCUST_MASTER+x} ] ; then
-        echo "You need to set LOCUST_MASTER."
+if [ ${1} = "locust" ] ; then 
+    echo "Starting locust......."
+    if [ -z ${LOCUST_HOST+x} ] ; then
+        echo "You need to set the URL of the host to be tested (LOCUST_HOST)."
         exit 1
     fi
-    ;;
-esac
-
-exec "$@" ${LOCUST_OPTS}
+fi
+exec "$@"
